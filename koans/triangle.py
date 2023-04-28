558196a4b789473571e5be59a47dbdd784fce101
Steve Tooke <steve@took.es>
2023-04-28 09:13:34 +0100

    Exceptions . and triangles

--- koans/triangle.py
+++ koans/triangle.py
@@ -17,8 +17,20 @@
 #   about_triangle_project_2.py
 #
 def triangle(a, b, c):
-    # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    unique_sides = {a, b, c}
+    if not all_sides_real(unique_sides) or invalid_triangle(a, b, c):
+        raise TriangleError()
+    elif len(unique_sides) == 1:
+        return 'equilateral'
+    elif len(unique_sides) == 2:
+        return 'isosceles'
+    return 'scalene'
+
+def all_sides_real(unique_sides):
+    return all(v > 0 for v in unique_sides)
+
+def invalid_triangle(a,b,c):
+    return (a + b <= c or a + c <= b or b + c <= a)
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
